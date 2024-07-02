aa0bbaf5f18dcf8f3cc6069e710bef62f9f4cb75
Abdallahelbaz <abdallah.elbaz19@gmail.com>
2024-07-02 15:46:12 +0200

    edit the whole project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cafateria-spring
+
+@
--- src/main/java/Repos/MenuRepo.java
+++
@@ -1,7 +0,0 @@
-package edu.abdullah.cafeteria.Repos;
-
-import edu.abdullah.cafeteria.model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepo extends JpaRepository<Menu,Long> {
-}
