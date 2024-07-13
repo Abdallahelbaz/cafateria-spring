9912066ef17d43d2b0c79d4009634482b56d8d12
Abdallahelbaz <abdallah.elbaz19@gmail.com>
2024-07-13 12:14:58 +0200

    added some changes

---
+++ src/main/java/edu/cafeteria/Controllers/CartController.java
@@ -0,0 +1,59 @@
+package edu.cafeteria.Controllers;
+import edu.cafeteria.model.Cart;
+import edu.cafeteria.model.Role;
+import edu.cafeteria.model.User;
+import edu.cafeteria.Services.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionFailedException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+@Controller
+@RequestMapping("/cart")
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+    @Autowired
+    private PaymentController paymentController;
+
+    @PostMapping("/add")
+    public String addToCart(@RequestParam Long itemId, HttpSession session) {
+        String userEmail = ((User) session.getAttribute("user")).getEmail();
+        cartService.addToCart(userEmail, itemId);
+        
+        return "redirect:/HomeStaff";
+    }
+    
+    @PostMapping("/addToGuest")
+    public String addToCartGuest(@RequestParam Long itemId, HttpSession session) {
+        String userEmail = ((User) session.getAttribute("user")).getEmail();
+        cartService.addToCart(userEmail, itemId);
+        
+        return "redirect:/HomeGuest";
+    }
+    
+    @GetMapping
+    public String viewCart(Model model, HttpSession session) {
+        String userEmail = ((User) session.getAttribute("user")).getEmail();
+        Cart cart = cartService.getCart(userEmail);
+       
+        paymentController.totalPrice=cart.getTotalPrice();//
+        paymentController.items=cart.getItems();
+        
+        model.addAttribute("userRole", ((User) session.getAttribute("user")).getRole().name());  
+        model.addAttribute("cart", cart);
+        return "cart";
+    }
+
+    @PostMapping("/checkout")
+    public String proceedToCheckout() {
+       // return "redirect:/payment";
+        return "redirect:/payment";
+    }
+}
---
+++ src/main/java/edu/cafeteria/converter/OrderConverter.java
@@ -0,0 +1,41 @@
+package edu.cafeteria.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import edu.cafeteria.DTO.OrderDTO;
+import edu.cafeteria.DTO.UserDTO;
+import edu.cafeteria.model.Order;
+import edu.cafeteria.model.User;
+
+@Component
+public class OrderConverter {
+
+	
+	
+	 public OrderDTO convertToDTO(Order order) {
+	        return new OrderDTO(order.getId(), order.getOrderDate(), order.getItems() 
+	        		,order.getTotalPrice() , order.getUser() , order.getStatus() );
+	    }
+
+	    
+	    public Order convertToEntity(OrderDTO orderDTO) {
+	        return new Order(orderDTO.getId(), orderDTO.getOrderDate(), orderDTO.getItems() 
+	        		,orderDTO.getTotalPrice(), orderDTO.getUser() , orderDTO.getStatus() );
+	    }
+	    
+	    public List<OrderDTO> modelToDto(List<Order> orderList) {
+			
+	        List<OrderDTO> orderDtoList = new ArrayList<>();
+	        for (Order order : orderList) {
+	            orderDtoList.add(convertToDTO(order));
+	        }
+	        
+	        return orderDtoList;
+	        
+	        
+	    }  
+	    
+}
